82c2c75a46224386ec13c0603e81c0a93dc03c7b
Wev-Qwaser <85594047+Wev-Qwaser@users.noreply.github.com>
2021-10-04 22:58:03 -0300

    Update config.php
    correção

--- config.php
+++ config.php
@@ -12,17 +12,5 @@ spl_autoload_register(function($class_name){
 
 
 =======
-<?php
 
-spl_autoload_register(function($class_name){
-    $filename ="classes".DIRECTORY_SEPARATOR.$class_name.".php";
-
-    if(file_exists(($filename))) {
-        require_once($filename);
-    }
-
-});
-
-
->>>>>>> 514a1cee1fbf649e70cb80636df7e7aecb541353
 ?>
