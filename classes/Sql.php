2fe1e4a501e0df6367a20f9a6e905bd03fedc6c4
Wev-Qwaser <85594047+Wev-Qwaser@users.noreply.github.com>
2021-10-04 22:59:31 -0300

    Update Sql.php

--- classes/Sql.php
+++ classes/Sql.php
@@ -43,48 +43,5 @@ class Sql extends PDO{
 
 
 =======
-<?php
 
-class Sql extends PDO{
-
-        private $conn;
-        
-        public function __construct(){
-            $this->conn = new PDO("mysql:host=localhost;dbname=dbphp8", "root","");
-            
-        }
-
-        private function setParams($statement , $parameters = array())
-        {
-            foreach($parameters as $key => $value) {
-                $this->setParam($statement,$key,$value);
-            }
-
-        }
-        private function setParam($statement, $key , $value){
-            $statement->bindParam($key, $value);
-        }
-        public function rQuery($rawQuery, $params = array()){
-            $stmt= $this->conn->prepare($rawQuery);
-
-            $this->setParams($stmt, $params);
-            
-            $stmt->execute();
-
-            return $stmt;
-        }
-
-        public function select($rawQuery , $params = array()):array
-        {
-            $stmt=$this->rquery($rawQuery , $params);
-
-            return $stmt->fetchAll(PDO::FETCH_ASSOC);
-        }
-
-
-}
-
-
-
->>>>>>> 514a1cee1fbf649e70cb80636df7e7aecb541353
 ?>
